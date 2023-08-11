a2b86f703da634c3072142a91fd1dd8424b82f97
IlyaMelnichenko <124725419+IlyaMelnichenko@users.noreply.github.com>
2023-08-11 23:40:39 +0300

    a

---
+++ src/components/Feedback/Feedback.js
@@ -0,0 +1,20 @@
+export function Feedback(){
+    return(
+        <>
+        <h2>Please leave feedback</h2>
+        <button>Good</button>
+        <button>Neutral</button>
+        <button>bad</button>
+        <h2>Statistics</h2>
+        <ul>
+            <li>Good:</li>
+            <li>Bad:</li>
+            <li>Neutral:</li>
+            <li>Total:</li>
+            <li>Positive feedback:</li>
+        </ul>
+        
+        </>
+        
+    )
+}
