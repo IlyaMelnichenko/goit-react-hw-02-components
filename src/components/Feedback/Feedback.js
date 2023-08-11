fdbeadb7228736f3a6f63e2e23541923ce5e57cb
IlyaMelnichenko <124725419+IlyaMelnichenko@users.noreply.github.com>
2023-08-12 00:59:13 +0300

    Update Feedback.js

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -1,36 +1,43 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-export class Feedback extends Component{
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-      }
-      onClickGood = () => {
-        this.setState(prevState => ({
-            console.log(this.state.good);
-          good: prevState.good + 1
-        }));
-      }
-    
+export class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  onClickGood = () => {
+    this.setState(prevState => ({
+      good: prevState.good + 1,
+    }));
+  };
+  onClickBad =()=>{
+    this.setState(prevState =>({
+      bad:prevState.bad+1,
+    }))
+  };
+  onClickNeutral =()=>{
+    this.setState(prevState =>({
+      neutral:prevState.neutral+1,
+    }))
+  };
 
-    render(){  return(
-        <>
+  render() {
+    return (
+      <>
         <h2>Please leave feedback</h2>
-        <button type = 'button' onClick={this.onClickGood} >Good</button>
-        <button onClick={this.onClickGood} >Neutral</button>
-        <button onClick={this.onClickGood} >bad</button>
+        <button type="button" onClick={this.onClickGood}>
+          Good
+        </button>
+        <button onClick={this.onClickNeutral}>Neutral</button>
+        <button onClick={this.onClickBad}>bad</button>
         <h2>Statistics</h2>
         <ul>
-            <li>Good:</li>
-            <li>Bad:</li>
-            <li>Neutral:</li>
+          <li>Good:{this.state.good}</li>
+          <li>Bad:{this.state.bad}</li>
+          <li>Neutral:{this.state.neutral}</li>
         </ul>
-        
-        </>
-        
-    )}
-
-
-  
+      </>
+    );
+  }
 }
